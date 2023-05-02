6735d5f56d41d5a768037d885a12de635ffbc1c5
ayaOaya <alyaauaacn@gmail.com>
2023-05-02 23:07:50 +0200

    added costume middleware 3rd party middlware && build in middleware logserr

---
+++ config/allowedOrigins.js
@@ -0,0 +1,7 @@
+const allowedOrigins = [
+    'http://localhost:3000',
+    'https://www.virtualmachine.com',
+    'https://virtualmachine.com'
+] // here we are determining what hosts have acces to our api
+
+module.exports = allowedOrigins
--- server.js
+++ server.js
@@ -1,4 +1,43 @@
-//const express = require("express");
-//const express = require("express");
-//const express = require("express");
-//const express = require("express");
+const express = require('express');
+const app = express();
+const path = require('path')
+const { logger } = require('./middleware/logger')
+const errorHandler = require('./middleware/errorHandler')
+const cookieParser = require('cookie-parser')
+const cors = require('cors')
+const corsOptions = require('./config/corsOptions')
+const PORT = process.env.PORT || 3500
+
+app.use(logger)
+
+// to protect routes?
+app.use(cors(corsOptions))
+
+// give us the abality to process json && let our app recieve json
+app.use(express.json())
+
+// this is a 3rd party middleware[to parse cookies that we receive ]
+app.use(cookieParser())
+
+
+// telling the app to look for the public folder
+app.use('/', express.static(path.join(__dirname, 'public'))) //express.static is middleware
+
+
+app.use('/', require('./routes/root.js'))
+
+// handelling 404 page  
+app.all('*', (req, res) => {
+    res.status(404)
+    if (req.accepts('html')) {
+        res.sendFile(path.join(__dirname, 'views', '404 .html'))
+    } else if (req.accepts('json')) {
+        res.json({ message: '404 Not Found'})
+    } else {
+        res.type('txt').send('404 Not Found')
+    }
+})
+
+app.use(errorHandler)
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))  

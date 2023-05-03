a41a605194b145b4648b47c925b43c14331bc143
ayaOaya <alyaauaacn@gmail.com>
2023-05-03 17:55:22 +0200

    creating usersController dir- adding methods-checking validation && notesContollers-rootNotes

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,13 +2,18 @@ const User = require('../models/User')
 const Note = require('../models/Note')
 const asyncHandler = require('express-async-handler')
 const bcrypt = require('bcrypt')
+const { arMA } = require('date-fns/locale')
 
 // @desc Get all users
 // @route Get /users
 // @access Private
 
 const getAllUsers = asyncHandler(async (req, res) => {
-    
+    const users = await User.find().select('-password').lean() // this line say to not return the password to the user
+    if (!users?.length) {
+        return res.status(400).json({ message: 'No users found'})
+    }
+    res.json(users) // if there is a user, return it
 })
 
 // @desc Create new user
@@ -16,7 +21,33 @@ const getAllUsers = asyncHandler(async (req, res) => {
 // @access Private
 
 const createNewUser = asyncHandler(async (req, res) => {
+    const { username, password, roles } = req.body
 
+    // Confirm data
+    if (!username || !password || !Array.isArray(roles) || !roles.length) {
+        return res.status(400).json({ message: 'All fields are required'})
+    }
+
+    // Check for duplicate
+    const duplicate = await User.findOne({ username }).lean().exec()
+
+    if (duplicate) {
+        return res.status(409).json({ message: 'Duplicate username'})
+    }
+
+    // Hash password 
+    const hashedPwd = await bcrypt.hash(password, 10) // salt rounds
+
+    const userObject = { username, "password": hashedPwd, roles}
+
+    // Create and store new user
+    const user = await User.create(userObject)
+
+    if (user) { //created
+        res.status(201).json({ message: `New user ${username} created`})
+    } else {
+        res.status(400).json({ message: 'Invalid user data received'})
+    }
 })
 
 // @desc Update a user
@@ -24,7 +55,39 @@ const createNewUser = asyncHandler(async (req, res) => {
 // @access Private
 
 const updateUser = asyncHandler(async (req, res) => {
+    const { id, username, roles, active, password } = req.body
 
+    // Confirm data
+    if (!id || !username || !Array.isArray(roles) || !roles.length || typeof active !== 'boolean') {
+        return res.status(400).json({ message: 'All fields are required'})
+    } 
+
+    const user = await User.findById(id).exec()
+
+    if (!user) {
+        return res.status(400).json({ message: 'User not found'})
+    }
+
+    // Check for duplicate
+    const duplicate = await User.findOne({ username }).lean().exec()
+    // Allow updates to the original user
+    if (duplicate && duplicate?._id.toString() !== id) {
+        return res.status(409).json({ message: 'Duplicate username'})
+    }
+
+    // these are the new updated username, roles and active status
+    user.username = username
+    user.roles = roles
+    user.active = active
+ 
+    if (password) {
+        // Hash password
+        user.password = await bcrypt.hash(password, 10) // salt rounds
+    }
+
+    const updatedUser = await user.save() // we re able to call the save method here because we didnt add lean() to the user, and we called it bc we need it
+
+    res.json({ message: `${updatedUser.username} updated`})
 })
 
 // @desc Delete a user
@@ -32,7 +95,28 @@ const updateUser = asyncHandler(async (req, res) => {
 // @access Private
 
 const deleteUser = asyncHandler(async (req, res) => {
+    const { id } = req.body
 
+    if (!id) {
+        return res.status(400).json({ message: 'User ID Required'})
+    }
+    
+    const note = await Note.findOne({ user: id}).lean().exec()
+    if (note ) {
+        return res.status(400).json({ message: 'User has assigned notes'})
+    }
+
+    const user = await User.findById(id).exec()
+
+    if (!user) {
+        return res.status(400).json({ message: 'User not found'})
+    }
+
+    const result = await user.deleteOne()
+
+    const reply = `Username ${result.username} with ID ${result._id} deleted`
+
+    res.json(reply)
 })
 
 module.exports = {

2b4cb0cea5d7963a5643fe8f851120211c02202c
ayaOaya <alyaauaacn@gmail.com>
2023-05-03 02:16:11 +0200

    adding the users route and linking them to users controllers && linking to mongo db

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
+require('dotenv').config() // to use enviroment variables 
 const express = require('express');
 const app = express();
 const path = require('path')
-const { logger } = require('./middleware/logger')
+const { logger, logEvents } = require('./middleware/logger')
 const errorHandler = require('./middleware/errorHandler')
 const cookieParser = require('cookie-parser')
 const cors = require('cors')
 const corsOptions = require('./config/corsOptions')
+const connectDB = require('./config/dbConn')
+const mongoose = require('mongoose')
 const PORT = process.env.PORT || 3500
+
+
+connectDB()
 
 app.use(logger)
 
@@ -25,6 +31,7 @@ app.use('/', express.static(path.join(__dirname, 'public'))) //express.static is
 
 
 app.use('/', require('./routes/root.js'))
+app.use('/users', require('./routes/userRoutes'))
 
 // handelling 404 page  
 app.all('*', (req, res) => {
@@ -33,11 +40,20 @@ app.all('*', (req, res) => {
         res.sendFile(path.join(__dirname, 'views', '404 .html'))
     } else if (req.accepts('json')) {
         res.json({ message: '404 Not Found'})
-    } else {
+    } else {   
         res.type('txt').send('404 Not Found')
     }
 })
 
 app.use(errorHandler)
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`))  
+
+mongoose.connection.once('open', () => {
+    console.log('Connected to MongoDB')
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+})
+
+mongoose.connection.on('error', err => {
+    console.log(err)
+    logEvents(`${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`, 'mongoErrLog.log')
+})
